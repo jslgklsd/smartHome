ad22fe49ddfb7b12e0787d92c1f2dceffe9cabc9
“Von <2160823797@qq.com>
2023-11-27 17:01:27 +0800

    1.实现了注册功能，把用户输入账号密码存储到test.xml 2.实现了用户输入账号密码与本地文件test.xml的账号密码判断，如果一致跳转到设备界面，如果不一致，提示用户账号密码错误 3.封装了各种api，优化了代码结构

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -27,100 +31,118 @@ public class LoginActivity extends AppCompatActivity {
     private Button btn_password;
     private EditText et_account;
     private Button btn_login;
-    private CheckBox cb;
+    private Button btn_register;
+    private View viewInflated;
+    private AlertDialog.Builder builder;
+    private SharedPreferences sp1;
+    private SharedPreferences.Editor spEditor1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        et_password = findViewById(R.id.et_password);
-        btn_password = findViewById(R.id.btn_password);
+        initializeViews();          // 控件初始化
+        createSharedPreferences();  // 创建接口 sp、spEditor
+
         btn_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Drawable currentIcon = btn_password.getBackground();
-                Drawable passwordHideIcon = getResources().getDrawable(R.drawable.password_hide);
-
-                if(currentIcon.getConstantState().equals(passwordHideIcon.getConstantState())) {
-                    btn_password.setBackgroundResource(R.drawable.password_visible);
-                    et_password.setInputType(InputType.TYPE_CLASS_NUMBER);
-                } else {
-                    btn_password.setBackgroundResource(R.drawable.password_hide);
-                    et_password.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
-                }
+                togglePasswordVisibility(btn_password, et_password);
             }
         });
 
-        et_account = findViewById(R.id.et_account);
-        btn_login = findViewById(R.id.btn_login);
-        cb = findViewById(R.id.cb);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String account = et_account.getText().toString();
-                String password = et_password.getText().toString();
-                if ((account.equals("Admin")) && (password.equals("12345678"))){    // 账号密码正确
-                    // 用户勾选了记住密码
-                    if (cb.isChecked())
-                        saveCredentials(account, password);
-
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                } else{
-                    Toast.makeText(LoginActivity.this, "账号或密码错误，请重新输入", Toast.LENGTH_SHORT).show();
-                }
+                login();
             }
         });
 
-        // 从本地文件中读取已保存的账号密码
-        String savedAccount = getSavedAccount();
-        String savedPassword = getSavedPassword();
-        if (!TextUtils.isEmpty(savedAccount) && !TextUtils.isEmpty(savedPassword)) {
-            et_account.setText(savedAccount);
-            et_password.setText(savedPassword);
-            cb.setChecked(true);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterDialog();   // 显示注册对话框
+            }
+        });
+    }
+
+    // 控件初始化
+    private void initializeViews() {
+        et_account = findViewById(R.id.et_account);
+        btn_login = findViewById(R.id.btn_login);
+        et_password = findViewById(R.id.et_password);
+        btn_password = findViewById(R.id.btn_password);
+        btn_register = findViewById(R.id.btn_register);
+    }
+
+    // 把本地文件test.xml中键对应的值读出来
+    public String getDataByKey(SharedPreferences sp, String key) {
+        return sp.getString(key, "");
+    }
+
+    // 创建接口 sp、spEditor
+    public void createSharedPreferences() {
+        sp1 = this.getSharedPreferences("test", MODE_PRIVATE);
+        spEditor1 = sp1.edit();
+    }
+
+    // 密码可见性切换
+    public void togglePasswordVisibility(Button button, EditText editText) {
+        Drawable currentIcon = button.getBackground();
+        Drawable passwordHideIcon = getResources().getDrawable(R.drawable.password_hide);
+
+        if (currentIcon.getConstantState().equals(passwordHideIcon.getConstantState())) {
+            button.setBackgroundResource(R.drawable.password_visible);
+            editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        } else {
+            button.setBackgroundResource(R.drawable.password_hide);
+            editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
         }
     }
 
-    // 保存账号密码到本地文件
-    private void saveCredentials(String account, String password) {
-        try {
-            FileOutputStream outputStream = openFileOutput("credentials.txt", Context.MODE_PRIVATE);
-            outputStream.write((account + "\n" + password).getBytes());
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void login() {
+        String account_et = et_account.getText().toString();
+        String password_et = et_password.getText().toString();
+
+        String sp_account = getDataByKey(sp1, "account");    // 以String Key 为索引来获取账号
+        String sp_password = getDataByKey(sp1, "password");  // 以String Key 为索引来获取密码
+
+        // 用户输入账号密码与本地文件存储的账号密码一致
+        if ((account_et.equals(sp_account)) && (password_et.equals(sp_password))) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(LoginActivity.this, "账号或密码错误，请重新输入", Toast.LENGTH_SHORT).show();
         }
     }
 
-    // 从本地文件中读取已保存的账号
-    private String getSavedAccount() {
-        try {
-            FileInputStream inputStream = openFileInput("credentials.txt");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String account = reader.readLine();
-            reader.close();
-            return account;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    public void showRegisterDialog() {
+        builder = new AlertDialog.Builder(LoginActivity.this);  // 创建一个AlertDialog.Builder
 
-    // 从本地文件中读取已保存的密码
-    private String getSavedPassword() {
-        try {
-            FileInputStream inputStream = openFileInput("credentials.txt");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            // 跳过第一行，读取第二行作为密码
-            reader.readLine();
-            String password = reader.readLine();
-            reader.close();
-            return password;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        // 设置对话框布局
+        viewInflated = LayoutInflater.from(LoginActivity.this).inflate(R.layout.dialog_register, null);
+        builder.setView(viewInflated);
+
+        // 添加OK按钮
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // 以键值 <String Key,String Value> 方式加入数据
+                EditText et_wangguan = viewInflated.findViewById(R.id.et_wangguan);
+                spEditor1.putString("account", et_wangguan.getText().toString());
+                EditText dialog_et_password = viewInflated.findViewById(R.id.et_password);
+                spEditor1.putString("password", dialog_et_password.getText().toString());
+                spEditor1.commit();
+                Toast.makeText(LoginActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+
+                dialog.dismiss();   // 关闭Dialog
+            }
+        });
+
+        // 显示Dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
